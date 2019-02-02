38d0be3e19d298ef7f04f313b38231ae18391d61
Leonid Svyatov <leonid@svyatov.ru>
2019-02-03 01:08:44 +0300

    mix.exs updated for publishing on hex.pm

--- mix.exs
+++ mix.exs
@@ -8,30 +8,32 @@ defmodule Deck.MixProject do
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-
-      # Docs
       name: "Deck",
       source_url: "https://github.com/svyatov/deck",
-      homepage_url: "https://github.com/svyatov/deck",
-      docs: [
-        main: "Deck",
-        extras: ["README.md"]
-      ]
+      description: description(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:benchee, "~> 0.13", only: :dev}
+    ]
+  end
+
+  defp description() do
+    "Get, shuffle and deal a deck of playing cards of any size (from 4 to 52 cards)."
+  end
+
+  defp package() do
+    [
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/svyatov/deck"}
     ]
   end
 end
