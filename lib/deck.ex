69a27450a59380000632079500419368195deca5
Isak Berglind <info@isakberglind.se>
2019-07-04 21:27:26 +0200

    changed T to 10

--- lib/deck.ex
+++ lib/deck.ex
@@ -5,8 +5,8 @@ defmodule Deck do
 
   import Deck.Guards, only: [is_valid_number_of_cards: 1]
 
-  # T = 10, J = Jack, Q = Queen, K = King, A = Ace
-  @ranks ~w[2 3 4 5 6 7 8 9 T J Q K A]
+  # J = Jack, Q = Queen, K = King, A = Ace
+  @ranks ~w[2 3 4 5 6 7 8 9 10 J Q K A]
 
   # Clubs, diamonds, hearts, spades
   @suits ~w[c d h s]
@@ -22,7 +22,7 @@ defmodule Deck do
       iex> Deck.new()
       ["2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
        "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s",
-       "8c", "8d", "8h", "8s", "9c", "9d", "9h", "9s", "Tc", "Td", "Th", "Ts",
+       "8c", "8d", "8h", "8s", "9c", "9d", "9h", "9s", "10c", "10d", "10h", "10s",
        "Jc", "Jd", "Jh", "Js", "Qc", "Qd", "Qh", "Qs", "Kc", "Kd", "Kh", "Ks",
        "Ac", "Ad", "Ah", "As"]
   """
